e12ab2d69d0cfa79d8bad7b187b26deea640915c
qaiserjofficial <qaiserjavedofficial@gmail.com>
2022-12-16 23:15:32 +0500

    AuthController Updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,28 +6,32 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const Email = require('../utils/email');
 
-const signToken = (id) => {
-  const createSendToken = (user, statreq, usCode, req, res) => {
-    const token = signToken(user._id);
-    // Send Cookies to the client
-    res.cookie('jwt', token, {
-      expires: new Date(
-        Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-      ),
-      httpOnly: true,
-      secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-    });
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-    // Remove the password from the output
-    user.password = undefined;
-    res.status(statusCode).json({
-      status: 'success',
-      token,
-      data: {
-        user,
-      },
-    });
-  };
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  res.cookie('jwt', token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
 };
 
 exports.signup = catchAsync(async (req, res, next) => {
