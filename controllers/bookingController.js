e83deaeb253821078c6207d65f69e63222374768
qaiserjofficial <qaiserjavedofficial@gmail.com>
2022-12-17 12:43:57 +0500

    quantity variable location restores

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -60,7 +60,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].price_data.unit_amount / 100;
-  console.log(price);
+  console.log('Price', price);
   await Booking.create({ tour, user, price });
 };
 
